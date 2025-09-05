0440043ba1e9966de47b53e718ad431a80235a12
rondoncamacho <rondoncamacho@gmail.com>
2025-09-05 14:38:47 -0300

    feat: agregando plugin de vite para react

--- consofacil-frontend/vite.config.js
+++ consofacil-frontend/vite.config.js
@@ -1,6 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
 });
